47e753b75f7e861fbecb7f2bb36f19dd060b00d4
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2018-12-13 19:12:02 +0800

    Travis update

--- .utility/update_demo_site.sh
+++ .utility/update_demo_site.sh
@@ -1,3 +1,51 @@
 #!/bin/bash
 set -ev
-echo -e "Testing"
+echo -e "Demo deploy started"
+
+if [[ -z "$GH_TOKEN" ]]; then
+echo -e "GH_TOKEN is not set"
+exit 1
+fi
+
+if [ ! -f $TRAVIS_BUILD_DIR/target/gmd-am4charts-demo-*.war ]; then
+echo -e "pattern war file not found."
+exit 1
+fi
+
+echo -e "Publishing pattern to gh-pages . . .\n"
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+
+# clone the gh-pages branch.
+cd $HOME
+rm -rf gh-pages
+git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/GwtMaterialDesign/gmd-am4charts-demo gh-pages > /dev/null
+cd gh-pages
+
+# remove the GwtMaterialDemo directories from git.
+if [[ -d ./GmdAm4ChartsDemo ]]; then
+git rm -rf ./GmdAm4ChartsDemo
+fi
+if [[ -f ./index.html ]]; then
+git rm -rf ./index.html
+fi
+if [[ -d ./META-INF ]]; then
+git rm -rf ./META-INF
+fi
+if [[ -d ./WEB-INF ]]; then
+git rm -rf ./WEB-INF
+fi
+
+# copy the new GWTMaterialPattern the snapshot dir.
+unzip -u $TRAVIS_BUILD_DIR/target/gmd-am4charts-demo-*.war -d ./
+rm -rf ./META-INF
+rm -rf ./WEB-INF
+
+git add -f .
+git commit -m "Auto-push pattern to gh-pages successful. (Travis build: $TRAVIS_BUILD_NUMBER)"
+git push -fq origin gh-pages > /dev/null
+
+echo -e "Published pattern to gh-pages.\n"
+
+fi
