c35189d4177406cbda23865b017657f53b1e9bdf
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-07-24 17:32:02 +0800

    2.4.0-rc1 Release

--- .utility/update_demo_site.sh
+++ .utility/update_demo_site.sh
@@ -24,12 +24,12 @@ git clone --quiet --branch=gh-pages https://$GH_TOKEN@github.com/GwtMaterialDesi
 cd gh-pages
 
 # remove the GmdAmCharts4Demo Snapshot directories from git.
-if [[ -d ./snapshot/ ]]; then
-git rm -rf ./snapshot/
+if [[ -d ./ ]]; then
+git rm -rf ./
 fi
 
 # copy the new GmdAmCharts4Demo the snapshot dir.
-unzip -u $TRAVIS_BUILD_DIR/target/GmdAmCharts4Demo*.war -d ./snapshot/
+unzip -u $TRAVIS_BUILD_DIR/target/GmdAmCharts4Demo*.war -d ./
 
 git add -f .
 git commit -m "Auto-push pattern to gh-pages successful. (Travis build: $TRAVIS_BUILD_NUMBER)"
