d4f9a2008ed11134093dbce0660df12c035e5de3
travis-ci <travis@travis-ci.org>
2020-08-10 11:39:52 +0000

    Auto-push pattern to gh-pages successful. (Travis build: 68)

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-var cacheName = 'cache_1595584748880';
+var cacheName = 'cache_1597059572223';
 
 var filesToCache = [  
 '/gmd-amcharts4-demo/',
 
-'GmdAmCharts4Demo/A798B32FF4A3A4BF77A1320A4CDF942F.cache.js',
+'GmdAmCharts4Demo/6AD59C9B63216992F7F217475B629E91.cache.js',
 
 'GmdAmCharts4Demo/GmdAmCharts4Demo.devmode.js',
 
